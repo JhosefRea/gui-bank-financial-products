ae8392eafcf11fc18f0ebfa79440919ae92ab3a6
JhosefRea <jhosefrea18jr17@gmail.com>
2025-06-07 00:04:44 -0500

    Corrijo lógica de paginaión, Añado funcionalidad de búsqueda. Añado lógica de mensajes de alerta con ngx-toastr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,22 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
+    provideToastr(
+      {
+        positionClass: 'toast-top-right',
+        closeButton: true,
+        preventDuplicates: true,
+        timeOut: 3000,
+        progressBar: true,
+      }
+    ),
+  ],
 };
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,55 +3,76 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
-
 import { FetchApiProductsService } from '../../services/fetch-api-products.service';
 import { ProductStateService } from '../../services/product-state.service';
 import { ProductDTO } from '../dtos/product.dto';
 import { ProductLabels, AppRoutes } from '../../shared/utils/enums/product.enum';
 import { extractColumnKeys } from '../../shared/utils/utils';
+import { SearchService } from '../../services/product-search.service';
 
 @Component({
+  standalone:true,
   selector: 'app-dashboard',
   imports: [CommonModule, FormsModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
+  template: `
+    <table>
+      <tr *ngFor="let item of filteredItems">
+        <td>{{ item }}</td>
+      </tr>
+    </table>
+  `,
+  styles: [],
 })
 export class DashboardComponent implements OnInit {
   products: ProductDTO[] = [];
   columnKeys: string[] = [];
   columnLabels: string[] = Object.values(ProductLabels);
+  filteredItems = [...this.products];
+  
 
   //EDIT
   openDropdownId: string | null = null;
 
   //PAGINATION
-  pageSizes: number[] = [5, 10, 20];
   itemsPerPage: number = 5;
-  paginatedProducts: ProductDTO[] = []; // Visibles según el select
+  currentPage: number = 1;
+  pageSizes: number[] = [5, 10, 20];
 
   constructor(
     private fetchApiProducts: FetchApiProductsService, 
     private router: Router,
-    private productStateService: ProductStateService
+    private productStateService: ProductStateService,
+    private searchService: SearchService
   ) {}
 
   ngOnInit(): void {
-    this.getProducts();
+    this.getProducts(); 
+    this.searchService.searchTerm$.subscribe((term) => {
+      if (term.trim() === '') {
+        this.filteredItems = [...this.products]; 
+      } else {
+        this.filteredItems = this.products.filter((item) =>
+          item.name.toLowerCase().includes(term.toLowerCase())
+        );
+      }
+    });
   }
-
+  
   getProducts(): void {
     this.fetchApiProducts.getAll().subscribe({
       next: (data: ProductDTO[]) => {
         this.products = data;
-          this.columnKeys = extractColumnKeys(data);
+        this.columnKeys = extractColumnKeys(data);      
+        this.filteredItems = [...this.products];
       },
       error: (err) => {
         console.error('Error fetching products:', err);
       },
     });
   }
-
-  // Mostrar u ocultar el dropdown del producto seleccionado
+  
   toggleDropdown(productId: string): void {
     this.openDropdownId = this.openDropdownId === productId ? null : productId;
   }
@@ -67,12 +88,30 @@ export class DashboardComponent implements OnInit {
   }
 
   // PAGINATION
-  onItemsPerPageChange(): void {
-    this.updatePaginatedProducts();
+
+  get paginatedItems(): any[] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    return this.filteredItems.slice(startIndex, startIndex + this.itemsPerPage);
+  }
+  
+  get totalPages(): number {
+    return Math.ceil(this.filteredItems.length / this.itemsPerPage);
+  }
+  
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+  
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
   }
 
-  updatePaginatedProducts(): void {
-    this.paginatedProducts = this.products.slice(0, this.itemsPerPage);
+  onItemsPerPageChange() {
+    this.currentPage = 1;
   }
   
 }
